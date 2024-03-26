59a12c6c7897d2bfe80b73bae5d489bb7bcfbb40
Isaque Bernardes <isaque.ribeiro.bernardes@gmail.com>
2024-03-25 23:35:05 -0300

    Uploading springboot starter project

---
+++ src/main/java/br/com/isaquebrb/onewaycryptography/OneWayCryptographyApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.onewaycryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneWayCryptographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneWayCryptographyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isaquebrb/onewaycryptography/OneWayCryptographyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.onewaycryptography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneWayCryptographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
